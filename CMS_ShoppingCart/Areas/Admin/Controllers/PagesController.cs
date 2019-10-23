a7da944cdacc6ed6aad6fb19a7621a92e5ca64e8
FarhadMohamad <art_ismat@hotmail.com>
2019-10-23 09:07:43 +0200

    Get and Post methods for Addpage

---
+++ CMS_ShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using CMS_ShoppingCart.Models.Data;
+using CMS_ShoppingCart.Models.ViewModels.Pages;
+
+namespace CMS_ShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of PageViewModel
+            List<PageViewModel> PagesList;
+
+            using (Db db = new Db())
+            {            
+                //Initialize the list
+                PagesList = db.Pages.ToArray().OrderBy(x => x.Sort).Select(x=> new PageViewModel(x)).ToList();
+
+            }
+            //Return view with list
+            return View(PagesList);
+        }
+        // GET: Admin/Pages/AddPages
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // GET: Admin/Pages/AddPages
+        [HttpPost]
+        public ActionResult AddPage(PageViewModel viewModel)
+        {
+            //Check model state
+            if (! ModelState.IsValid)
+            {
+                return view(viewModel);
+            }
+
+            using (Db db = new Db())
+            {
+                //Declare slug
+                string slug;
+
+                //Initialize pageDTO
+                PageDTO dto = new PageDTO();
+
+                //DTO title
+                dto.Title = viewModel.Title;
+
+                //Check for and set slug if needed
+                if (string.IsNullOrWhiteSpace(viewModel.Slug))
+                {
+                    slug = viewModel.Title.Replace("", "-").ToLower();
+                }
+                else
+                {
+                    slug = viewModel.Slug.Replace("", "-").ToLower();
+                }
+                //Make sure title and slug are unique
+                if (db.Pages.Any(x=>x.Title == viewModel.Title) || db.Pages.Any(x=>x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Slug and title with this name already exists");
+                    return View(viewModel);
+                }
+
+                //DTO the rest
+                dto.Slug = slug;
+                dto.Body = viewModel.Body;
+                dto.Sidebar = viewModel.Sidebar;
+                //Whenever you add a page will be the last page
+                dto.Sort = 100;
+                //Save the DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+
+            }
+            //Set TempData message
+            TempData["SM"] = "You have added a new page!";
+            
+            //Redirect
+
+
+            return RedirectToAction("AddPage");
+        }
+    }
+}
---
+++ CMS_ShoppingCart/Models/ViewModels/Pages/PageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using CMS_ShoppingCart.Models.Data;
+
+namespace CMS_ShoppingCart.Models.ViewModels.Pages
+{
+    public class PageViewModel
+    {
+        public PageViewModel()
+        {
+            
+        }
+        public PageViewModel(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sort = row.Sort;
+            Sidebar = row.Sidebar;
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+   
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public int Sort { get; set; }
+        public bool Sidebar { get; set; }
+    }
+}
