77072d13f50040949b5c743c5892053f34f05090
FarhadMohamad <art_ismat@hotmail.com>
2019-11-05 07:49:05 +0100

    EditPage done

--- CMS_ShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMS_ShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -11,35 +11,35 @@ namespace CMS_ShoppingCart.Areas.Admin.Controllers
 {
     public class PagesController : Controller
     {
-        // GET: Admin/Pages
+        // GET: Admin Pages
         public ActionResult Index()
         {
             //Declare list of PageViewModel
             List<PageViewModel> PagesList;
 
             using (Db db = new Db())
-            {            
+            {
                 //Initialize the list
-                PagesList = db.Pages.ToArray().OrderBy(x => x.Sort).Select(x=> new PageViewModel(x)).ToList();
+                PagesList = db.Pages.ToArray().OrderBy(x => x.Sort).Select(x => new PageViewModel(x)).ToList();
 
             }
             //Return view with list
             return View(PagesList);
         }
-        // GET: Admin/Pages/AddPages
+        // GET: Admin AddPages
         public ActionResult AddPage()
         {
             return View();
         }
 
-        // GET: Admin/Pages/AddPages
+        // POST: Admin AddPages
         [HttpPost]
         public ActionResult AddPage(PageViewModel viewModel)
         {
             //Check model state
-            if (! ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                return view(viewModel);
+                return View(viewModel);
             }
 
             using (Db db = new Db())
@@ -56,14 +56,14 @@ namespace CMS_ShoppingCart.Areas.Admin.Controllers
                 //Check for and set slug if needed
                 if (string.IsNullOrWhiteSpace(viewModel.Slug))
                 {
-                    slug = viewModel.Title.Replace("", "-").ToLower();
+                    slug = viewModel.Title.Replace(" ", "-").ToLower();
                 }
                 else
                 {
-                    slug = viewModel.Slug.Replace("", "-").ToLower();
+                    slug = viewModel.Slug.Replace(" ", "-").ToLower();
                 }
                 //Make sure title and slug are unique
-                if (db.Pages.Any(x=>x.Title == viewModel.Title) || db.Pages.Any(x=>x.Slug == slug))
+                if (db.Pages.Any(x => x.Title == viewModel.Title) || db.Pages.Any(x => x.Slug == slug))
                 {
                     ModelState.AddModelError("", "Slug and title with this name already exists");
                     return View(viewModel);
@@ -82,11 +82,128 @@ namespace CMS_ShoppingCart.Areas.Admin.Controllers
             }
             //Set TempData message
             TempData["SM"] = "You have added a new page!";
-            
+
             //Redirect
 
 
             return RedirectToAction("AddPage");
         }
+
+        // GET: Admin EditPage by id
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            //Declaring pageVM
+            PageViewModel pageViewModel;
+            using (Db db = new Db())
+            {
+                //Getting the page
+                PageDTO pDTO = db.Pages.Find(id);
+                //Confirming if the page exists
+                if (pDTO == null)
+                {
+                    return Content("The page does not exist. ");
+                }
+
+                //Intializing pageVM
+                pageViewModel = new PageViewModel(pDTO);
+            }
+
+            //Returning view with model
+            return View(pageViewModel);
+        }
+
+        //POST: Admin EditPage by id
+        [HttpPost]
+        public ActionResult EditPage(PageViewModel pageViewModel)
+        {
+            //Check for model state
+            if (!ModelState.IsValid)
+            {
+                return View(pageViewModel);
+            }
+            using (Db db = new Db())
+            {
+                //Get page id
+                int id = pageViewModel.Id;
+
+
+                //Initializing slug
+                string slug = "home";
+                //Getting the page
+                PageDTO pageDTO = db.Pages.Find(id);
+
+                //DTO the title
+                pageDTO.Title = pageViewModel.Title;
+                //Checking for slug and set it if needed
+                if (pageViewModel.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(pageViewModel.Slug))
+                    {
+                        slug = pageViewModel.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = pageViewModel.Slug.Replace(" ", "-").ToLower();
+
+                    }
+
+                }
+                //Make sure that the title and slug are unique
+
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == pageViewModel.Title) ||
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+
+                {
+                    ModelState.AddModelError("", "The title or slug by this name already exists");
+                    return View(pageViewModel);
+                }
+
+                //DTO the rest
+                pageDTO.Slug = slug;
+                pageDTO.Body = pageViewModel.Body;
+                pageDTO.Sidebar = pageViewModel.Sidebar;
+
+
+                //Save the DTO
+                db.SaveChanges();
+                //Set TempData message
+                TempData["SM"] = "page edited successfully";
+                //Redirect
+                return RedirectToAction("EditPage");
+            }
+
+
+        }
+
+        // GET: Admin PageDetails by id
+        public ActionResult PageDetails(int id)
+        {
+            //Declaring PageViewModel
+            PageViewModel pageViewModel;
+            using (Db db = new Db())
+            {
+                //Getting the page
+                PageDTO dto = db.Pages.Find(id);
+                //Confirming if page exists
+                if (dto == null)
+                {
+                    return Content("This page does not exist.");
+                }
+                //Initializing PageViewModel
+                //pageViewModel = new PageViewModel(dto)
+                //paged
+
+            }
+
+
+
+            //Returning view with model
+
+
+            return View();
+        }
+
+
     }
 }
